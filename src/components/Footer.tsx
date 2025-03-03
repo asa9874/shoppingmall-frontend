dacdb60aa53a79146d0a1584290c39ed9007d190
asa9874 <pjkpjk1129@gmail.com>
2025-03-03 21:31:34 +0900

    Feat: 푸터 컴포넌트 스타일 개선 및 저작권 정보 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,22 @@
 
 function Footer(){
     return(
-        <div className="h-[100px]">푸터</div>
+        <footer className="bg-white shadow-sm dark:bg-gray-900 ">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2025 asa9874. All Rights Reserved.
+        </span>
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <a
+            href="https://github.com/asa9874"
+            className="text-blue-500 hover:underline"
+          >
+            https://github.com/asa9874
+          </a>
+        </span>
+      </div>
+    </footer>
     )
 }
 
